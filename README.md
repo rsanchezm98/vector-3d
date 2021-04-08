c43a4ed31ac044487f78dde9013e961e69dca120
rsanchezm98 <rsanchezm98@gmail.com>
2021-04-08 12:03:34 +0200

    updates

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # 3D vector C++ class
 A simple class implementation of a 3d vector [x,y,z] in C++.
 
-## Build
-Just put in the terminal:
-```
-make
-```
-If new .cpp files are written, you must include them in the makefile
+# 3D vector utils
+There is also implemented a template library that makes operations with the Vector3D class. 
 
+## Build
+It is included a makefile so as to only write on terminal: *make* and then the programs will get built.
+
+## GTEST verification
+To check the vector_utils.hpp functions, there is implemented a test.cpp which uses google test to check its validity.
+
+## Previous requirements
+You need to have correctly installed gtest in order to use it. You must build the test program as done in the makefile provided. 
--- test.cpp
+++ test.cpp
@@ -2,24 +2,13 @@
 #include "vector_utils.hpp"
 #include <iostream>
 #include "gtest/gtest.h"
-#include <math.h>
- 
-double squareRoot(const double a) {
-    double b = sqrt(a);
-    if(b != b) { // nan check
-        return -1.0;
-    }else{
-        return sqrt(a);
-    }
-}
 
 TEST(Prueba1, a)
 {
-    EXPECT_EQ(6, squareRoot(36.0));
-    EXPECT_EQ(18.0, squareRoot(324.0));
-    EXPECT_EQ(25.4, squareRoot(645.16));
-    EXPECT_EQ(0, squareRoot(0.0));
+    EXPECT_EQ(6, 2*3);
+
 }
+
 /*
 TEST(DotProductTest, allTests) { 
     Vector3D myVector1(1.0,2.0,3.0);
@@ -33,9 +22,10 @@ TEST(DotProductTest, allTests) {
 
 int main (int argc, char* argv[])
 {
-    std::cout << "***************** TESTING *****************" << std::endl;
+    std::cout << "***************** Init Google Test () *****************" << std::endl;
     testing::InitGoogleTest(&argc, argv);
 
+    std::cout << "***************** Run All Tests () *****************" << std::endl;
     return RUN_ALL_TESTS();
 
 }
